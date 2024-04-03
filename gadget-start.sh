4cc90b5b87830f530d9edf726e6e9e9a4efb61e8
dafta <dafta007@gmail.com>
2024-04-03 21:16:14 +0200

    Add Steam Deck defaults for UDC

--- gadget-start.sh
+++ gadget-start.sh
@@ -3,19 +3,7 @@ set -e
 cd "${GADGET}"
 case "${1}" in
 	start)
-		if [ "${UDC}" == "auto" ]
-		then	if _UDC="$(dmidecode -s system-serial-number)"
-			then	echo "set udc to ${_UDC}"
-				echo "${_UDC}" > UDC
-			else	for i in /sys/class/udc/*
-				do	[ -h "${i}" ]||continue
-					_UDC="$(basename "$i")"||continue
-					echo "set udc to ${_UDC}"
-					echo "${_UDC}" > UDC
-					exit
-				done
-			fi
-		fi
+		echo "${UDC}" > UDC
 	;;
 	stop)
 		echo "disable gadget"
